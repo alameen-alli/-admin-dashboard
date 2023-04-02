33dbe8eb91295a36dbee043969684c9694f777b9
unknown <allialameen8739@gmail.com>
2023-04-02 10:39:00 +0100

    context

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import { FiSettings } from 'react-icons/fi';
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components'
 // import { Area, Bar, ColorManager, Financial, Line, Pie, Pyramid, Stacked, ColorMapping } from './pages/Charts';
-import {Calendar, ColorPicker, Customers, Ecommerce, Editor, Employees, Kanban, Orders } from './pages'
+import { Calendar, ColorPicker, Customers, Ecommerce, Editor, Employees, Kanban, Orders } from './pages'
 import { useStateContext } from './contexts/ContextProvider';
 
 function App() {
@@ -38,43 +38,47 @@ function App() {
 
           <div className={
             activeMenu
-             ? 'dark:bg-main-dark-bg bg-main-bg min-h-screen md:ml-72 w-full' 
-             : 'bg-main-bg dark:main-dark-bg  w-full min-h-screen flex-2'
-            }
+              ? 'dark:bg-main-dark-bg bg-main-bg min-h-screen md:ml-72 w-full'
+              : 'bg-main-bg dark:main-dark-bg  w-full min-h-screen flex-2'
+          }
           >
             <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
               <Navbar />
             </div>
 
-          <div>
-            <Routes>
-              {/* Dashboard */}
-              <Route path='/' element={<Ecommerce/>} />
-              <Route path='/ecommerce' element={<Ecommerce/>} />
+            <div>
 
-              {/* Pages */}
-              <Route path='/orders' element={<Orders />} />
-              <Route path='/employees' element={<Employees />} />
-              <Route path='/customers' element={<Customers />} />
+              <ThemeSettings />
 
-              {/* Apps */}
-              <Route path='/kanban' element={<Kanban />} />
-              <Route path='/editor' element={<Editor />} />
-              <Route path='/calendar' element={<Calendar />} />
-              <Route path='/color-picker' element={<ColorPicker/>} />
+              <Routes>
 
-              {/* Charts */}
-              <Route path='/line' element="Line" />
-              <Route path='/area' element="Area" />
-              <Route path='/bar' element="Bar" />
-              <Route path='/pie' element="Pie" />
-              <Route path='/financial' element="Financial" />
-              <Route path='/color-mapping' element="Color-Mapping" />
-              <Route path='/pyramid' element="" />
-              <Route path='/stacked' element="Stacked" />
-            </Routes>
+                {/* Dashboard */}
+                <Route path='/' element={<Ecommerce />} />
+                <Route path='/ecommerce' element={<Ecommerce />} />
+
+                {/* Pages */}
+                <Route path='/orders' element={<Orders />} />
+                <Route path='/employees' element={<Employees />} />
+                <Route path='/customers' element={<Customers />} />
+
+                {/* Apps */}
+                <Route path='/kanban' element={<Kanban />} />
+                <Route path='/editor' element={<Editor />} />
+                <Route path='/calendar' element={<Calendar />} />
+                {/* <Route path='/color-picker' element={<ColorPicker />} /> */}
+
+                {/* Charts */}
+                <Route path='/line' element="Line" />
+                <Route path='/area' element="Area" />
+                <Route path='/bar' element="Bar" />
+                <Route path='/pie' element="Pie" />
+                <Route path='/financial' element="Financial" />
+                <Route path='/color-mapping' element="Color-Mapping" />
+                <Route path='/pyramid' element="" />
+                <Route path='/stacked' element="Stacked" />
+              </Routes>
+            </div>
           </div>
-        </div>
         </div>
       </BrowserRouter>
     </div>
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -13,16 +13,39 @@ export const StateProvider = ({ children }) => {
     const [activeMenu, setActiveMenu] = useState(true);
     const [isClicked, setIsClicked] = useState(initialState);
     const [screenSize, setScreenSize] = useState('');
+    const [currentColor, setCurrentColor] = useState('#03C9D7');
+    const [currentMode, setCurrentMode] = useState('Light');
+    const [themeSettings, setThemeSettings] = useState('false');
+
+    const setMode = (e) => {
+        setCurrentMode(e.target.value);
+
+        localStorage.setItem('themeMode', e.target.value);
+    }
+
+    const setColor = (e) => {
+        setCurrentColor(e.target.value);
+
+        localStorage.setItem('themeMode', e.target.value);
+    }
 
     const handleClick = (clicked) => { 
         setIsClicked({...initialState, [clicked] : true});
     }
 
     return (
-        <StateContext.Provider value={{ activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize }}>
+        <StateContext.Provider value={{ 
+            activeMenu, setActiveMenu, 
+            isClicked, setIsClicked, 
+            handleClick, screenSize, setScreenSize ,
+            currentColor, setCurrentColor,
+            currentMode, setCurrentMode,
+            themeSettings, setThemeSettings,
+            setColor, setMode
+        }}>
             {children}
         </StateContext.Provider>
     )
 };
 
-export const useStateContext = () => useContext(StateContext);
+export const useStateContext = () => useContext(StateContext);e
